5c7c59731350be270669373d20aa63bda5a75685
Daniel Johns <daniel.johns@robotsandpencils.com>
2014-02-12 15:23:57 -0700

    Setup tableview test

--- TableViewTest/LNViewController.h
+++ TableViewTest/LNViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LNViewController : UIViewController
+@interface LNViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
--- TableViewTest/LNViewController.m
+++ TableViewTest/LNViewController.m
@@ -20,10 +20,33 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    //dont use red because that is the background color we're using to show the gap
+    UIColor *color;
+    do {
+        color = [self randomColor];
+    } while (color == [UIColor redColor]);
+    
+    cell.backgroundColor = color;
+    
+    return cell;
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return @[@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
+}
+
+- (UIColor *)randomColor{
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
 }
 
 @end
